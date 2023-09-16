2cf10eaf84a99161bcdd473c2c54603292947429
amiixana <145202064+amiixana@users.noreply.github.com>
2023-09-16 15:22:23 +0300

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,13 @@
+let swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    loop: true
+});
+
